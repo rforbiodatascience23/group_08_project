9d30c87fdd2bc6a07ddab58ff7be0e0755e635e7
s233139 <s233139@student.dtu.dk>
2023-11-27 00:13:35 +0100

    Tydiesd graph plotting, both the function and its application

--- R/99_proj_func.R
+++ R/99_proj_func.R
@@ -1,6 +1,7 @@
 # Function to create and save the graph of a community to a PNG file
-create_and_save_graph <- function(graph_data, prefix, i, output_dir) {
-  current_graph <- graph_data$communities[[i]]
+create_and_save_graph_for_community <- function(community_data, type, subcommunity, seed, output_dir) {
+  i <- subcommunity
+  current_graph <- community_data$communities[[i]]
   
   # Plot the graph
   g <- ggraph(current_graph) +
@@ -10,6 +11,8 @@ create_and_save_graph <- function(graph_data, prefix, i, output_dir) {
     ggtitle(paste("Community", i))
   
   # Save the graph as a PNG file
-  graph_filename <- file.path(output_dir, str_c(prefix, "_community_", i, ".png"))
+  graph_filename <- file.path(output_dir, str_c(type, "_community_", i, ".png"))
   ggsave(graph_filename, g, width = 10, height = 8)
 }
+
+
